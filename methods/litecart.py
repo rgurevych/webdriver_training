cdb0faf13b5969959cdf24231d2726e1c8306434
Rostyslav Gurevych <rostikg@gmail.com>
2017-03-21 14:32:28 +0100

    Homework 7 - Check of header presence for all menu and submenu items

---
+++ methods/litecart.py
@@ -0,0 +1,28 @@
+
+def login_to_admin(wd):
+    wd.get("http://localhost/litecart/admin/")
+    wd.find_element_by_name("username").send_keys("admin")
+    wd.find_element_by_name("password").send_keys("admin")
+    wd.find_element_by_name("login").click()
+    assert not wd.current_url.endswith("/login.php")
+
+
+def logout_from_admin(wd):
+    wd.find_element_by_css_selector("i.fa.fa-sign-out").click()
+    assert wd.current_url.endswith("/login.php")
+
+
+def check_left_menu_elements(wd):
+    menu_list = wd.find_elements_by_css_selector("li#app-")
+    for i in range(0, len(menu_list)):
+        wd.find_elements_by_css_selector("li#app-")[i].click()
+        assert len(wd.find_elements_by_css_selector("td#content h1")) == 1
+        submenu_list = wd.find_elements_by_css_selector("li#app-")[i].find_elements_by_css_selector("li")
+        if len(submenu_list)>0:
+            check_left_sumbenu_elements(wd, i, len(submenu_list))
+
+
+def check_left_sumbenu_elements(wd, i, submenu_length):
+    for j in range(0, submenu_length):
+        wd.find_elements_by_css_selector("li#app-")[i].find_elements_by_css_selector("li")[j].click()
+        assert len(wd.find_elements_by_css_selector("td#content h1")) == 1
--- tests/test_left_menu.py
+++ tests/test_left_menu.py
@@ -1,21 +1,18 @@
 import pytest
 from selenium import webdriver
 from methods import litecart
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 
 @pytest.fixture
 def wd(request):
     wd = webdriver.Chrome()
+    #wd.implicitly_wait(1)
     #wd = webdriver.Firefox(capabilities={"marionette": True}, firefox_binary ='C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe')
-    #print(wd.capabilities)
     request.addfinalizer(wd.quit)
     return wd
 
 
-def test_login(wd):
+def test_left_menu_headers_presence(wd):
     litecart.login_to_admin(wd)
-
-
-
-
+    litecart.check_left_menu_elements(wd)
+    litecart.logout_from_admin(wd)
